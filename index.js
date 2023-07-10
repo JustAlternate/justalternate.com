24734223645a34b475989d16324ce87a34fb6581
JustAlternate <lololollololol890@gmail.com>
2023-07-10 19:56:51 +0200

    finally fixing viewport widht and height being wrong on touch screen device

--- index.js
+++ index.js
@@ -168,8 +168,8 @@ var neptune_orbital_speed = Math.random()/100
 function resizeRendererToDisplaySize(renderer) {
   const canvas = renderer.domElement;
   const pixelRatio = window.devicePixelRatio;
-  const width  = canvas.clientWidth  * pixelRatio | 0;
-  const height = canvas.clientHeight * pixelRatio | 0;
+  const width  = window.screen.width * pixelRatio | 0;
+  const height = window.screen.height * pixelRatio | 0;
   const needResize = canvas.width !== width || canvas.height !== height;
   if (needResize) {
     renderer.setSize(width, height, false);
